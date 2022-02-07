f4ef4e39321d568b8d894fe7cafabfa57921c441
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-07 14:46:06 +0000

    Extensions complete

--- src/main/java/DebitCard.java
+++ src/main/java/DebitCard.java
@@ -1,21 +1,16 @@
-public class DebitCard implements IScan {
-    private String cardNumber;
+public class DebitCard extends Card implements IScan {
     private int sortCode;
     private int accountNumber;
-    private String expiryDate;
-    private int securityNumber;
+
 
     public DebitCard(String cardNumber, int sortCode, int accountNumber, String expiryDate, int securityNumber) {
-        this.cardNumber = cardNumber;
+        super(cardNumber, expiryDate, securityNumber);
         this.sortCode = sortCode;
         this.accountNumber = accountNumber;
-        this.expiryDate = expiryDate;
-        this.securityNumber = securityNumber;
+
     }
 
-    public String getCardNumber() {
-        return this.cardNumber;
-    }
+
 
     public int getSortCode() {
         return this.sortCode;
@@ -25,13 +20,7 @@ public class DebitCard implements IScan {
         return this.accountNumber;
     }
 
-    public String getExpiryDate() {
-        return this.expiryDate;
-    }
 
-    public int getSecurityNumber() {
-        return this.securityNumber;
-    }
 
     public String scan() {
         return "Payment Complete";
