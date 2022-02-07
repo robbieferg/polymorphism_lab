830f091d7aa084370b940f31705431453974c408
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-07 14:37:35 +0000

    MVP complete

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -2,17 +2,12 @@ import java.util.ArrayList;
 
 public class Wallet {
     private String name;
-    private ArrayList<CreditCard> creditCards;
-    private ArrayList<DebitCard> debitCards;
-    private ArrayList<LoyaltyCard> loyaltyCards;
-    private ArrayList<Ticket> tickets;
+    private ArrayList<IScan> items;
+
 
     public Wallet(String name) {
         this.name = name;
-        this.creditCards = new ArrayList<CreditCard>();
-        this.debitCards = new ArrayList<DebitCard>();
-        this.loyaltyCards = new ArrayList<LoyaltyCard>();
-        this.tickets = new ArrayList<Ticket>();
+        this.items = new ArrayList<IScan>();
     }
 
     public String getName() {
@@ -20,22 +15,11 @@ public class Wallet {
     }
 
     public int getNumberOfItems() {
-        return this.creditCards.size() + this.debitCards.size() + this.loyaltyCards.size() + this.tickets.size();
+        return this.items.size();
     }
 
-    public void addItem(CreditCard item) {
-        this.creditCards.add(item);
+    public void addItem(IScan item) {
+        this.items.add(item);
     }
-
-    public void addItem(DebitCard item) {
-        this.debitCards.add(item);
-    }
-
-    public void addItem(LoyaltyCard item) {
-        this.loyaltyCards.add(item);
-    }
-
-    public void addItem(Ticket item) {
-        this.tickets.add(item);
-    }
+    
 }
